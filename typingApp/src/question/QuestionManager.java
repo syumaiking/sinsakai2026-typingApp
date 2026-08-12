fcddab43eb6b6b3bb7cbc2597e2e34d5e8a326de
syumaiking <noguchisyudai0310@gmail.com>
2026-08-12 14:16:13 +0900

    QuestionManagerクラスの追加

---
+++ typingApp/src/question/QuestionManager.java
@@ -0,0 +1,12 @@
+package question;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionManager {
+	private List<Question> questions;
+
+	public QuestionManager() {
+		questions = new ArrayList<>();
+	}
+}
